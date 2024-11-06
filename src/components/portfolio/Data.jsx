ee7a9775a20ec998a9d3affab7c98585c0c3f9cf
Ajmaldevala <ajmaldevala@gmail.com>
2024-11-06 11:50:32 +0530

    chore : Social Meadia

---
+++ src/components/portfolio/Data.jsx
@@ -0,0 +1,35 @@
+import work1 from "../../assets/fundfolio.jpg";
+import work2 from "../../assets/foundr.png";
+import work3 from "../../assets/ride.png";
+import work4 from "../../assets/netflix.png";
+
+export const projectsData = [
+  {
+    id: 1,
+    image: work1,
+    title: "Fundfolio",
+    subtitle: "Personal Finance and Note Organizer",
+    link: 'https://fundfolio.vercel.app/'
+  },
+  {
+    id: 2,
+    image: work2,
+    title: "Foundr",
+    subtitle: "Entrepreneur Networking Platform",
+    link: 'https://www.foundr.site'
+  },
+  {
+    id: 3,
+    image: work3,
+    title: "Ride",
+    subtitle: "E-commerce Application",
+    link: 'https://www.livetoride.website'
+  },
+  {
+    id: 4,
+    image: work4,
+    title: "Netfilx Clone",
+    subtitle: "Netflix clone using TMDB API",
+    link: 'https://netflixx-clone.netlify.app/'
+  },
+];
