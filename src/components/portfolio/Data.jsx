41d1685e33611ad1a392f48d579d32507b6938d5
Ajmaldevala <ajmaldevala@gmail.com>
2024-11-15 14:08:09 +0530

    core : updated

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -1,9 +1,25 @@
 import work1 from "../../assets/fundfolio.jpg";
+import Centa from "../../assets/Centa.png";
 import work2 from "../../assets/foundr.png";
 import work3 from "../../assets/ride.png";
 import work4 from "../../assets/netflix.png";
+import Mahaseel from "../../assets/Mahaseel.jpeg";
 
 export const projectsData = [
+  {
+    id: 1,
+    image: Centa,
+    title: "Centa",
+    subtitle: "Centre for Teacher Accreditation",
+    link: 'https://centa.org/'
+  },
+  {
+    id: 3,
+    image: Mahaseel,
+    title: "Mahaseel",
+    subtitle: "Marketing and Agricultural Services",
+    link: 'https://www.mahaseel.net/en-GB'
+  },
   {
     id: 1,
     image: work1,
@@ -24,6 +40,13 @@ export const projectsData = [
     title: "Ride",
     subtitle: "E-commerce Application",
     link: 'https://www.livetoride.website'
+  },
+  {
+    id: 4,
+    image: work4,
+    title: "Netfilx Clone",
+    subtitle: "Netflix clone using TMDB API",
+    link: 'https://netflixx-clone.netlify.app/'
   },
   {
     id: 4,
