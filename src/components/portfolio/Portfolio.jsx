e674ecb569a4bd48cc3f497d989457b3106c36ab
Ajmaldevala <ajmaldevala@gmail.com>
2024-11-17 19:05:33 +0530

    chore : portfollio added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,16 +1,39 @@
-import Works from "./Works"
-import './portfolio.css'
+import { Helmet } from "react-helmet-async";
+import Works from "./Works";
+import "./portfolio.css";
 
 const Portfolio = () => {
   return (
-    <section className="work section"  id='portfolio'>
+    <>
+      <Helmet>
+        <title>Ajmal U - Full Stack Developer | MERN Stack</title>
+        <meta
+          name="description"
+          content="Ajmal U, a full-stack developer specializing in building scalable web applications and responsive interfaces using the MERN stack."
+        />
+        <meta
+          name="keywords"
+          content="Ajmal U, Full Stack Developer, MERN Stack, React, Node.js, MongoDB, JavaScript, ajmaldevala ,devala"
+        />
+        <meta
+          property="og:title"
+          content="Ajmal U - Full Stack Developer | MERN Stack"
+        />
+        <meta
+          property="og:description"
+          content="Portfolio of Ajmal U, showcasing skills in full-stack development with the MERN stack."
+        />
+        <meta property="og:type" content="website" />
+      </Helmet>
 
+      <section className="work section" id="portfolio">
         <h2 className="section__title">Portfolio</h2>
         <span className="section__subtitle">Most recent works</span>
 
         <Works />
-    </section>
-  )
-}
+      </section>
+    </>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
