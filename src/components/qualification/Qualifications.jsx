489484fa20d95edfadda3f450aa8ef23ceba7759
Ajmaldevala <ajmaldevala@gmail.com>
2024-11-26 16:57:21 +0530

    Docs : Qualification section  Design Changed

--- src/components/qualification/Qualifications.jsx
+++ src/components/qualification/Qualifications.jsx
@@ -1,7 +1,9 @@
 import { Helmet } from "react-helmet-async";
+import { useState } from "react";
 import "./qualification.css";
-
 const Qualifications = () => {
+  const [activeTab, setActiveTab] = useState("experience");
+
   return (
     <>
       <Helmet>
@@ -12,7 +14,7 @@ const Qualifications = () => {
         />
         <meta
           name="keywords"
-          content="Ajmal U, Full Stack Developer, MERN Stack, React, Node.js, MongoDB, JavaScript , ajmaldevala ,devala"
+          content="Ajmal U, Full Stack Developer, MERN Stack, React, Node.js, MongoDB, JavaScript, ajmaldevala, devala"
         />
         <meta
           property="og:title"
@@ -27,72 +29,126 @@ const Qualifications = () => {
 
       <section className="qualification section" id="qualification">
         <h2 className="section__title">Qualification</h2>
-        {/* <span className="section__subtitle">My personal journey</span> */}
 
-        <div className="qualification__container container">
+        <div className="qualification__container">
+          {/* Tabs */}
           <div className="qualification__tabs">
-            <div className="qualification__button button--flex">
-              <i className="uil uil-graduation-cap qualification__icon"></i>{" "}
+            <div
+              className={`qualification__button ${
+                activeTab === "experience" ? "qualification__active" : ""
+              }`}
+              onClick={() => setActiveTab("experience")}
+            >
+              <i className="uil uil-briefcase qualification__icon"></i>
+              Experience
+            </div>
+
+            <div
+              className={`qualification__button ${
+                activeTab === "education" ? "qualification__active" : ""
+              }`}
+              onClick={() => setActiveTab("education")}
+            >
+              <i className="uil uil-graduation-cap qualification__icon"></i>
               Education
             </div>
           </div>
 
-          <div className="qualification__section">
-            <div className="qualification__content">
-              {/* MERN Stack Development */}
-              <div className="qualification__data">
-                <div className="qualification__align-right">
-                  <h3 className="qualification__title">
-                    Full Stack Development
-                  </h3>
-                  <span className="qualification__subtitle">Brototype</span>
-                  <div className="qualification__calendar">
-                    <i className="uil uil-calendar-alt"></i> August 2022 - June
-                    2023
-                  </div>
-                </div>
-                <div>
-                  <span className="qualification__rounder"></span>
-                  <span className="qualification__line"></span>
+          {/* Experience Content */}
+          <div
+            className={`qualification__content ${
+              activeTab === "experience" ? "qualification__content-active" : ""
+            }`}
+          >
+            {/* Ampcome Technologies */}
+            <div className="qualification__data">
+              <div className="qualification__align-right">
+                <h3 className="qualification__title">Software Engineer</h3>
+
+                <a
+                  href="https://www.ampcome.com/"
+                  className="qualification__subtitle"
+                >
+                  Ampcome Technologies Pvt. Ltd.
+                </a>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> June 2023 - Present
                 </div>
               </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div></div>
+            </div>
 
-              {/* MSc (Computer Science) */}
-              <div className="qualification__data">
-                <div></div>
-                <div>
-                  <span className="qualification__rounder"></span>
-                  <span className="qualification__line"></span>
-                </div>
-                <div>
-                  <h3 className="qualification__title">
-                    MSc (Computer Science)
-                  </h3>
-                  <span className="qualification__subtitle">
-                    Bharathiar University
-                  </span>
-                  <div className="qualification__calendar">
-                    <i className="uil uil-calendar-alt"></i> 2020 - 2022
-                  </div>
+            {/* Brototype */}
+            <div className="qualification__data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div className="qualification__info">
+                <h3 className="qualification__title">Full Stack Development</h3>
+                <a
+                  href="https://www.brototype.com/"
+                  className="qualification__subtitle"
+                >
+                  Brototype
+                </a>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> August 2022 - June
+                  2023
                 </div>
               </div>
+            </div>
+          </div>
 
-              {/* BSc (Computer Science) */}
-              <div className="qualification__data">
-                <div className="qualification__align-right">
-                  <h3 className="qualification__title">
-                    BSc (Computer Science)
-                  </h3>
-                  <span className="qualification__subtitle">
-                    Bharathiar University
-                  </span>
-                  <div className="qualification__calendar">
-                    <i className="uil uil-calendar-alt"></i> 2017 - 2020
-                  </div>
+          {/* Education Content */}
+          <div
+            className={`qualification__content ${
+              activeTab === "education" ? "qualification__content-active" : ""
+            }`}
+          >
+            {/* MSc */}
+            <div className="qualification__data">
+              <div className="qualification__align-right">
+                <h3 className="qualification__title">MSc (Computer Science)</h3>
+                <a
+                  href="https://b-u.ac.in/"
+                  className="qualification__subtitle"
+                >
+                  Bharathiar University
+                </a>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> 2020 - 2022
                 </div>
-                <div>
-                  <span className="qualification__rounder"></span>
-                  <span className="qualification__line"></span>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div></div>
+            </div>
+
+            {/* BSc */}
+            <div className="qualification__data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div className="qualification__info">
+                <h3 className="qualification__title">BSc (Computer Science)</h3>
+                <a
+                  href="https://b-u.ac.in/"
+                  className="qualification__subtitle"
+                >
+                  Bharathiar University
+                </a>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> 2017 - 2020
                 </div>
               </div>
             </div>
