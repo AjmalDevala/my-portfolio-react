3c76f3aff98de2f92b5a15136d9674362921e726
Ajmaldevala <ajmaldevala@gmail.com>
2024-12-28 15:25:15 +0530

    Core : infor Page Updated

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -1,24 +1,23 @@
-
 const Info = () => {
   return (
     <div className="about__info grid">
-        <div className="about__box">
-        <i className='bx bx-award about__icon'></i>
-            <h3 className="about__title">Experience</h3>
-            <span className="about__subtitle">2 Years</span>
-        </div>
-        <div className="about__box">
-        <i className='bx bx-briefcase-alt about__icon' ></i>
-            <h3 className="about__title">Completed</h3>
-            <span className="about__subtitle">10+ Projects</span>
-        </div>
-        <div className="about__box">
-        <i className='bx bx-support about__icon' ></i>
-            <h3 className="about__title">Support</h3>
-            <span className="about__subtitle">Online 24/7</span>
-        </div>
+      <div className="about__box">
+        <i className="bx bx-award about__icon"></i>
+        <h3 className="about__title">Professional Experience</h3>
+        <span className="about__subtitle">2+ Years in Development</span>
+      </div>
+      <div className="about__box">
+        <i className="bx bx-briefcase-alt about__icon"></i>
+        <h3 className="about__title">Projects Delivered</h3>
+        <span className="about__subtitle">10+ High-Quality Solutions</span>
+      </div>
+      <div className="about__box">
+        <i className="bx bx-support about__icon"></i>
+        <h3 className="about__title">Client Support</h3>
+        <span className="about__subtitle">Available 24/7 Globally</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Info
+export default Info;
